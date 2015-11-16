12b99d6c89abb2d15990c080900d873ffd3e8b6e
Markus Heidelberg <markus.heidelberg@web.de>
2015-11-16 21:50:03 +0100

    Add a "usemaps" UI panel, accessible via button from toolbar
    This panel contains a list of the configured and enabled destination
    maps.
    
    Still rudimentary and unstyled.
    
    The icon is created by the author himself.

---
+++ data/usemaps.js
@@ -0,0 +1,79 @@
+/*
+ * UI panel associated to the whole site.
+ * It consists of links to the destination maps.
+ */
+
+/**
+ * Event to pass GeoHack compatible template URI to the Add-On.
+ *
+ * @event linkpress
+ * @type {String}
+ */
+
+/**
+ * Event to pass the required panel size to the Add-On.
+ * Takes 2 parameters: width, height
+ *
+ * @event pagesize
+ */
+
+
+/**
+ * Handle a click on a panel link.
+ *
+ * @param {Event} ev - click event
+ *
+ * @fires linkpress
+ */
+function handleLinkClick(ev)
+{
+	ev.preventDefault();
+	self.port.emit("linkpress", this.href);
+}
+
+/**
+ * Add a link to the panel.
+ *
+ * @param {String} name - link name
+ * @param {String} uri - link URI
+ */
+function addLink(name, uri)
+{
+	var item = document.createElement("li");
+	var link = document.createElement("a");
+
+	link.textContent = name;
+	link.href = uri;
+	link.addEventListener("click", handleLinkClick, false);
+
+	item.appendChild(link);
+	document.getElementById("listcontainer").appendChild(item);
+}
+
+/**
+ * Add links for all enabled destination maps to the panel.
+ *
+ * @param {DestinationsMaps} destMaps - destination maps
+ *
+ * @fires pagesize
+ */
+function createLinks(destMaps)
+{
+	var listcontainer = document.getElementById("listcontainer");
+
+	// Empty the link list first
+	while (listcontainer.firstChild) {
+		listcontainer.firstChild.remove();
+	}
+
+	// Recreate the link list
+	for (var i = 0; i < destMaps.length; i++) {
+		if (destMaps[i].enabled)
+			addLink(destMaps[i].name, destMaps[i].templateurl);
+	}
+
+	// Post new size
+	self.port.emit("pagesize", document.body.scrollWidth, document.body.scrollHeight);
+}
+
+self.port.on("destmaps", createLinks);
--- index.js
+++ index.js
@@ -28,6 +28,9 @@ var contextMenuitems = [];
 
 var geourl = require("./lib/geourl.js");
 
+var showmapButton;
+var usemapsPanel;
+
 /**
  * Set default destination maps in the preferences if not yet defined or empty.
  *
@@ -112,6 +115,80 @@ function createContextMenu()
 	}
 }
 
+/**
+ * Create the toggle button for opening destination maps from the current
+ * website.
+ *
+ * The button is visible all the time, in disabled or enabled state depending
+ * on whether the website is detected as a supported source.
+ */
+function createShowmapButton()
+{
+	var { ToggleButton } = require("sdk/ui/button/toggle");
+
+	showmapButton = ToggleButton({
+		id: "showmapButton",
+		label: "Show map",
+		icon: {
+			"16": "./icon-16.png",
+			"32": "./icon-32.png",
+			"64": "./icon-64.png"
+		}
+	});
+
+	// Show or hide the panel when clicking the button.
+	showmapButton.on("click", function(state) {
+		if (state.checked) {
+			usemapsPanel.show();
+		} else {
+			usemapsPanel.hide();
+		}
+	});
+}
+
+/**
+ * Create the panel with the list of links for opening destination maps from
+ * the current website.
+ *
+ * The panel is shown on click of the toggle button.
+ *
+ * @fires destmaps
+ * @listens linkpress
+ * @listens pagesize
+ */
+function createUsemapsPanel()
+{
+	usemapsPanel = require("sdk/panel").Panel({
+		contentURL: "./usemaps.html",
+		contentScriptFile: "./usemaps.js",
+		position: showmapButton
+	});
+
+	// Send the current destination maps to the panel script when showing
+	// the panel.
+	usemapsPanel.on("show", function() {
+		usemapsPanel.port.emit("destmaps", JSON.parse(preferences.prefs.destmaps));
+	});
+
+	// Update the button state when hiding the panel.
+	usemapsPanel.on("hide", function() {
+		showmapButton.state("window", {checked: false});
+	});
+
+	usemapsPanel.port.on("linkpress", function(url) {
+		var coords = geourl.parse(tabs.activeTab.url);
+		if (coords) {
+			tabs.open(geourl.decode(url, coords));
+		}
+		usemapsPanel.hide();
+	});
+
+	usemapsPanel.port.on("pagesize", function(w, h) {
+		// The width was reduced by 16 px on each change, so keep the default.
+		usemapsPanel.resize(usemapsPanel.width, h + 30);
+	});
+}
+
 function main(options, callbacks)
 {
 	initDestMaps();
@@ -119,6 +196,9 @@ function main(options, callbacks)
 	preferences.on("editmaps", createAndShowEditmaps);
 
 	createContextMenu();
+
+	createShowmapButton();
+	createUsemapsPanel();
 }
 
 exports.main = main;
