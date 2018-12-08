db64b7165bf77201db6c82baf621f59daed098d3
Markus Heidelberg <markus.heidelberg@web.de>
2018-12-08 12:37:21 +0100

    Add a new module for GeoHack coordinate calculations
    The new function mapcoords.complete() fills in missing elements of the
    GeoHackCoords object.
    
    For now the new {scale} element is supported and will be calculated from
    {osmzoom} and vice versa.

---
+++ test/test-mapcoords.js
@@ -0,0 +1,11 @@
+var mapcoords = require("../lib/mapcoords.js");
+
+var testdata = require("./testcoords.json");
+
+exports["test complete coords"] = function(assert) {
+	for (var i = 0; i < testdata.length; i++) {
+		assert.deepEqual(mapcoords.complete(testdata[i].partial), testdata[i].completed);
+	}
+};
+
+require("sdk/test").run(exports);
