marcows/showmap

commit af45e767f7192a85b1a71db8b51fdc33308cc414

Enable/disable the globe button separately for each browser window
Now it does not always have the same state for all open browser windows, but is
dependent on the active tab for each window.

Just cosmetics, didn't have influence for the usage.

index.js
@@ -190,11 +190,15 @@ function createUsemapsPanel()
 function updateShowmapButtonState()
 {
 	if (geourl.parse(tabs.activeTab.url)) {
-		showmapButton.disabled = false;
-		showmapButton.icon = iconsEnabled;
+		showmapButton.state("window", {
+			disabled: false,
+			icon: iconsEnabled
+		});
 	} else {
-		showmapButton.disabled = true;
-		showmapButton.icon = iconsDisabled;
+		showmapButton.state("window", {
+			disabled: true,
+			icon: iconsDisabled
+		});
 		usemapsPanel.hide();
 	}
 }

commit 620b10dd165b25c9317c85fd88f9c4117beadb44

Fix regression with undesired disabled globe button
If the usemaps panel was visible and you did not click on a map item,
but somewhere else, the panel was hidden as usual, but the globe button
was disabled although the tab still contained the same supported map.
This wasn't caused by setShowmapButtonState(), this function was not
invoked. For some reason the global shomwapButton.disabled state was
used (the bad behavior didn't occur when changing it to true in
setShowmapButtonState()).

Setting the disabled state tab- instead of window-specific fixes this
issue.

Regression introduced in commit af45e76 (Enable/disable the globe button
separately for each browser window, 2018-12-21).

index.js
@@ -184,11 +184,11 @@ function createUsemapsPanel()
 function updateShowmapButtonState()
 {
 	if (geourl.parse(tabs.activeTab.url)) {
-		showmapButton.state("window", {
+		showmapButton.state("tab", {
 			disabled: false
 		});
 	} else {
-		showmapButton.state("window", {
+		showmapButton.state("tab", {
 			disabled: true
 		});
 		usemapsPanel.hide();