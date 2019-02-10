d249c5ae1621202f14f69ee9887041a8574535f1
Markus Heidelberg <markus.heidelberg@web.de>
2019-02-10 19:36:15 +0100

    geosite: support delayed DOM creation of the Flickr photo site
    This fixes problem no. 2 for Flickr from the commit adding support for
    it: When a photo site is loaded the first time, the EXIF DOM part is not
    included immediately and the globe button stayed in disabled state.

--- data/geosite/flickr.js
+++ data/geosite/flickr.js
@@ -12,15 +12,57 @@
  */
 function scanGeosite_Flickr()
 {
-	var geoinfo;
-	var link;
+	/*
+	 * Search for Flickr map link
+	 *
+	 * @returns {GeositeInfo} Geo information, undefined if nothing found
+	 */
+	function findGeoinfo()
+	{
+		var geoinfo;
+		var link;
 
-	link = document.querySelector(".map-container a[href]");
+		link = document.querySelector(".map-container a[href]");
 
-	if (link) {
-		geoinfo = {};
-		geoinfo.url = link.href;
+		if (link) {
+			geoinfo = {};
+			geoinfo.url = link.href;
+		}
+
+		return geoinfo;
 	}
+
+	/*
+	 * Set up observer to search when the EXIF related DOM element has been
+	 * created.
+	 *
+	 * @fires geositeinfo
+	 */
+	function observeGeoinfo()
+	{
+		var callback = function(mutationsList, observer) {
+			for (var mutation of mutationsList) {
+				if (mutation.type === "childList") {
+					var geoinfo = findGeoinfo();
+					if (geoinfo) {
+						observer.disconnect();
+						self.port.emit("geositeinfo", geoinfo);
+					}
+				}
+			}
+		};
+		var observer = new MutationObserver(callback);
+		var content = document.querySelector("div.photo-charm-exif-scrappy-view");
+		var config = { childList: true };
+
+		if (content)
+			observer.observe(content, config);
+	}
+
+	var geoinfo = findGeoinfo();
+
+	if (!geoinfo)
+		observeGeoinfo();
 
 	return geoinfo;
 }
